14bb70327f8dd4345ba5fcf058f153f24036267b
2Ric <2.ric@mail.ru>
2022-12-17 15:29:48 +0600

    two components and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import {Auth} from "./Authorization/auth";
+import {Login} from "./Loginization/login";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+            <div className="App">
+                <Routes>
+
+                <Route path='/auth' element={<Auth/>}/>
+                <Route path='/login' element={<Login/>}/>
+                </Routes>
+
+            </div>
+
+    )
 }
 
 export default App
